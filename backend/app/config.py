0fe61e61b8cf5af7511c59cad6964745823bf7da
anmolsingh0219 <91377955+anmolsingh0219@users.noreply.github.com>
2024-12-21 22:48:27 +0530

    backend setup

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     AWS_SECRET_ACCESS_KEY: str
     AWS_REGION: str
     S3_BUCKET_NAME: str
-    BACKEND_CORS_ORIGINS: list = ["http://localhost:5173" , "https://mask-canvas.vercel.app"]
+    BACKEND_CORS_ORIGINS: list = ["*"]
 
     class Config:
         env_file = ".env"
--- backend/app/main.py
+++ backend/app/main.py
@@ -14,8 +14,8 @@ app = FastAPI()
 
 # Setup CORS
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+     CORSMiddleware,
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
